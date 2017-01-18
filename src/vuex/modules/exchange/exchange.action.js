640738d614520c8f3abd26d3b8172996327a511e
qianchen <qianchen245@gmail.com>
2017-01-18 18:25:53 +0800

    ignoreTimeoutRequest

--- src/vuex/modules/exchange/exchange.action.js
+++ src/vuex/modules/exchange/exchange.action.js
@@ -4,10 +4,12 @@ import * as test from './exchange.types.js'
 const actions = {
 	getExchange ({commit}, bank) {
 		api.getInfoByBank(bank).then(res => {
-			commit(test.GET_EXCHANGE, {
-				bank: bank,
-				exchanges: res.data
-			})
+			if (res.data.status === 200) {
+				commit(test.GET_EXCHANGE, {
+					bank: bank,
+					exchanges: res.data
+				})
+			}
 		})
 	}
 }
