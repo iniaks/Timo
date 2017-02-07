4f681310aa77770ea91f23802da6e3902b7161c2
qianchen <qianchen245@gmail.com>
2017-02-07 10:27:15 +0800

    SwitchServerApi

--- src/config/base.js
+++ src/config/base.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://49.51.10.135/' : 'http://localhost:3000/'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://106.75.29.82/' : 'http://localhost:3000/'
