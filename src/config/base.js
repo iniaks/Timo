1c4443657bfa04ec6f4162916be45d7dcd86c6b7
qianchen <qianchen245@gmail.com>
2017-09-29 16:04:40 +0800

    apiChange

--- src/config/base.js
+++ src/config/base.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://api.iniaks.com/' : 'http://localhost:3000/'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://api.tomqian.com/' : 'http://localhost:3000/'
